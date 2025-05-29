65d2478ef52e171f26d032b680965676b6621e08
Arfat Alam <117762576+Arfatalam@users.noreply.github.com>
2025-05-29 13:32:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+ // 1. javascript console API
+//  console.log("hello world")
+//  document.write("this is document .write")
+//  console.error("this is an error")
+//  console.warn("this is warning")
+
+//  javascript variable
+
+// 2. what are variables = container to store data values 
+// var no1=34;
+// var no2=56;
+// console.log(no1+no2)
+
+// 4. data type in javascript
+// string
+// var str1="this is string";
+// var str2="This is also a string";
+
+// number
+// var no1=56;
+// var no2=34;
+
+// objects
+// var marks= {
+//     ravi: 34,
+//     harry: 40,
+//     arfat: 48,
+// }
+// console.log(marks)
+
+// boolean
+// var a=true;
+// var b=false;
+// console.log(a,b)
+// var und = undefined;
+// console.log(und);
+
+// var n=null;
+// console.log(null)
+
+
+// at a very high level, there are two types of dara types in javascript
+// 1.primitive data typeo: undefined,null,number,string,booleansymbol
+// 2.reference data type:arrays and objects
+
+// var arr =[1,2,"bablu",4,5]
+// console.log(arr)
+
+// var a=34;
+// var b=56;
+// console.log("the value of a+b is",a+b);
+// console.log(typeof(a+b));
+// console.log(10+ "20")
+// console.log(9-"5")
+// console.log("java"+"script")
+// console.log("java"-"script")
+
+
+//          interview question
+// difference between null vs undefined
+
+// var iamuseless;
+// console.log(iamuseless)
+// console.log(typeof(iamuseless))
+
+var myno=752632727;
+var name="arfat";
+console.log(isNaN(myno));
+console.log(isNaN(name));
